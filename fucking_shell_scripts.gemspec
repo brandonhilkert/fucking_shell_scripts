ad3eced9f8e0e4a1e63d77e73943c9cb98f6624c
Brandon Hilkert <brandonhilkert@gmail.com>
2013-08-17 21:38:23 -0400

    Update specs

--- fucking_shell_scripts.gemspec
+++ fucking_shell_scripts.gemspec
@@ -21,6 +21,7 @@ Gem::Specification.new do |spec|
   spec.add_development_dependency "bundler", "~> 1.3"
   spec.add_development_dependency "rake"
   spec.add_development_dependency "rspec"
+  spec.add_development_dependency "pry"
 
   spec.add_dependency "fog", "~> 1.14.0"
 end
--- spec/fucking_shell_scripts/cli_spec.rb
+++ spec/fucking_shell_scripts/cli_spec.rb
@@ -2,6 +2,47 @@ require 'spec_helper'
 
 module FuckingShellScripts
   describe CLI do
+    let(:server) { double('server', bootstrap: true, build: true, configure: true) }
+    let(:options) {
+      {
+        type: "test-server",
+        region: "us-east-1",
+      }
+    }
+    let(:config) { double('config', options: options) }
 
+    it "initializes a server with the connection" do
+      connection = double('connection')
+      connection_obj = double('obj connection', connection: connection)
+      FuckingShellScripts::Connection.stub(:new).and_return(connection_obj)
+      FuckingShellScripts::Configuration.stub(:new).and_return(config)
+      FuckingShellScripts::Server.should_receive(:new).with(connection, options)
+      FuckingShellScripts::CLI.new.bootstrap
+    end
+
+    context "instance methods" do
+      before :each do
+        ENV["AWS_ACCESS_KEY"] = "key"
+        ENV["AWS_SECRET_ACCESS_KEY"] = "secret"
+        FuckingShellScripts::Configuration.stub(:new).and_return(config)
+        FuckingShellScripts::Server.stub(:new).and_return(server)
+      end
+
+      it "bootstraps a server" do
+        expect(server).to receive(:bootstrap)
+        FuckingShellScripts::CLI.new.bootstrap
+      end
+
+      it "builds a server" do
+        expect(server).to receive(:build)
+        FuckingShellScripts::CLI.new.build
+      end
+
+      it "configures a server" do
+        expect(server).to receive(:configure)
+        FuckingShellScripts::CLI.new.configure
+      end
+
+    end
   end
 end
